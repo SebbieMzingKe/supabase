7a6389aeb0985828c91f4a5f3fa6e8566d8441de
Sebbie Evayo <115040489+SebbieMzingKe@users.noreply.github.com>
2025-09-22 11:06:53 +0300

    add paragraph for testing Git submodules

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -23,6 +23,7 @@ const SmoothieCard = ({ smoothie, onDelete }) => {
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
+  <p>trying out gitsubmodules</p>
       <div className="rating">{smoothie.rating}</div>
       <div className="buttons">
         <Link to={"/" + smoothie.id}>
